d4c0f0738c4b30b555c37a6e69689c7ccd5eeb35
BappySheikh1 <sheikhbappy91gmail.com>
2023-07-09 17:24:03 +0600

    Checkout and cart done

--- src/redux/features/cart/cartSlice.tsx
+++ src/redux/features/cart/cartSlice.tsx
@@ -4,10 +4,12 @@ import { PayloadAction } from '@reduxjs/toolkit';
 
 interface ICart {
   products: IProduct[];
+  total: number;
 }
 
 const initialState: ICart = {
   products: [],
+  total: 0,
 };
 
 const cartSlice = createSlice({
@@ -24,6 +26,8 @@ const cartSlice = createSlice({
       } else {
         state.products.push({ ...action.payload, quantity: 1 });
       }
+
+      state.total += action.payload.price;
     },
     removeOneFromCart: (state, action: PayloadAction<IProduct>) => {
       const existing = state.products.find(
@@ -32,19 +36,24 @@ const cartSlice = createSlice({
 
       if (existing && existing.quantity! > 1) {
         existing.quantity = existing.quantity! - 1;
-      } else{
+      } else {
         state.products = state.products.filter(
           (product) => product._id !== action.payload._id
         );
       }
+
+      state.total -= action.payload.price;
     },
     removeFromCart: (state, action: PayloadAction<IProduct>) => {
       state.products = state.products.filter(
         (product) => product._id !== action.payload._id
       );
+
+      state.total -= action.payload.price * action.payload.quantity!;
     },
   },
 });
 
-export const { addToCart, removeFromCart,removeOneFromCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, removeOneFromCart } =
+  cartSlice.actions;
 export default cartSlice.reducer;
