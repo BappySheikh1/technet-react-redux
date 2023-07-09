1e9621829ebf17a1eac84017be2e3a87fbfcb3a3
BappySheikh1 <sheikhbappy91gmail.com>
2023-07-09 23:38:05 +0600

    rtk quary added

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducers from './features/cart/cartSlice';
+import productReducer from './features/products/productSlice';
+import {api} from './api/apiSlice'
 
 const store = configureStore({
   reducer: {
     cart: cartReducers,
+    product : productReducer,
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefaultMiddleware)=>getDefaultMiddleware().concat(api.middleware)
 });
 
 export type RootState = ReturnType<typeof store.getState>;
